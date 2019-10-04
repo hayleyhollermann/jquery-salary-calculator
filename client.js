cc4824ae8144ceaf17cb01263d1c0a64eae3636c
Hayley Hollermann <hayleyhollermann@gmail.com>
2019-10-04 17:07:42 -0500

    function appends new employee info to table, delete button removes

--- client.js
+++ client.js
@@ -3,7 +3,8 @@ console.log('js', $);
 $(document).ready(readyNow);
 
 function readyNow(){
-    $('#addEmployee').on('click', addEmployeeClick)
+    $('#addEmployee').on('click', addEmployeeClick);
+    $('tbody').on('click', 'button', deleteButtonClick);
 }
 
 function addEmployeeClick(){
@@ -14,4 +15,18 @@ function addEmployeeClick(){
     let title = $('#titleInput').val();
     let annualSalary = $('#annualSalaryInput').val();
     console.log(firstName, lastName, idNumber, title, annualSalary);
+    $('tbody').append(
+        `<tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${idNumber}</td>
+            <td>${title}</td>
+            <td>${annualSalary}</td>
+            <td><button class="deleteButton">Delete</button></td>
+        </tr>`
+    )
+}
+
+function deleteButtonClick(){
+    $(this).closest('tr').remove();
 }
